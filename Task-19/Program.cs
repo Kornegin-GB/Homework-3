ea78a2d25a1bca1524aa3c3f5d30f5c82451860b
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-14 19:28:48 +0300

    Решение задачи 19

--- Task-19/Program.cs
+++ Task-19/Program.cs
@@ -4,3 +4,42 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+using System;
+using static System.Console;
+
+Clear();
+
+bool DefPolendrome(string number, int first, int Last)
+{
+   bool isPolendrome = true;
+
+   while (first < Last)
+   {
+      if (number[first] == number[Last])
+      {
+         first++;
+         Last--;
+      }
+      else
+      {
+         isPolendrome = false;
+         break;
+      }
+   }
+   return isPolendrome;
+}
+
+int numberSigns = 5;
+int first = 0;
+int Last = numberSigns - 1;
+
+Write($"Введите число {numberSigns} значное: ");
+string? palindrome = ReadLine();
+
+if (palindrome?.Length != numberSigns) WriteLine($"Вы ввели не {numberSigns} значное число!");
+else
+{
+   if (DefPolendrome(palindrome, first, Last)) WriteLine("Палиндром");
+   else WriteLine("НЕ палиндром");
+}
