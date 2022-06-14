f599f8112db1f8c222e1dcb74df79b0e64c91b89
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-14 20:07:55 +0300

    Решение задачи 21

--- Task-21/Program.cs
+++ Task-21/Program.cs
@@ -3,3 +3,31 @@
 
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите координаты точки А:");
+Write("X = ");
+int Ax = Convert.ToInt32(ReadLine());
+Write("Y = ");
+int Ay = Convert.ToInt32(ReadLine());
+Write("Z = ");
+int Az = Convert.ToInt32(ReadLine());
+
+
+WriteLine("Введите координаты точки B:");
+Write("X = ");
+int Bx = Convert.ToInt32(ReadLine());
+Write("Y = ");
+int By = Convert.ToInt32(ReadLine());
+Write("Z = ");
+int Bz = Convert.ToInt32(ReadLine());
+
+int X = Bx - Ax, Y = By - Ay, Z = Bz - Az;
+
+double distans = Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+
+WriteLine($"Расстояние между точками {distans:f2}");
