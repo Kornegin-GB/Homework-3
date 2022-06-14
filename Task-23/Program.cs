b7f950e2902d7142ad5802d2b74e03d9da3677a0
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-14 20:20:09 +0300

    Решение задачи 23

--- Task-23/Program.cs
+++ Task-23/Program.cs
@@ -3,3 +3,19 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+
+WriteLine($"Таблица чисел от 1 до {number} в кубе");
+
+for (int count = 1; count <= number; count++)
+{
+   if (count != number) Write($"{Math.Pow(count, 3)}, ");
+   else Write($"{Math.Pow(count, 3)}\n");
+}
